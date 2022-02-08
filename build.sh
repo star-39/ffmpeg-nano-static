29e081b0d4c56778c65b381d97f3ea4219d563f4
star-39 <75669297+star-39@users.noreply.github.com>
2022-02-08 22:47:21 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ cd
 curl -Lo ffmpeg.tar.gz "https://git.ffmpeg.org/gitweb/ffmpeg.git/snapshot/master.tar.gz" && \
 mkdir ffmpeg && tar -xvf ffmpeg.tar.gz --strip-components=1 -C ffmpeg
 
-curl -Lo x264.tar.gz "https://code.videolan.org/videolan/x264/-/archive/master/x264-master.tar.gz" && \
-mkdir x264 && tar -xvf x264.tar.gz --strip-components=1 -C x264
+curl -Lo libvpx.tar.gz "https://chromium.googlesource.com/webm/libvpx/+archive/refs/heads/master.tar.gz" && \
+mkdir libvpx && tar -xvf libvpx.tar.gz -C libvpx
 
 cd
 
@@ -16,8 +16,8 @@ export LDFLAGS="-L/usr/lib -L/usr/lib64 -L/usr/local/lib -L/usr/local/lib64"
 export PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig" 
 
 
-cd x264 
-./configure --enable-static --disable-cli 
+cd libvpx
+./configure --disable-shared --disable-unit-tests --disable-docs --disable-tools --disable-examples --disable-vp8
 make -j$(nproc)
 sudo make install
 cd
@@ -32,7 +32,7 @@ cd ffmpeg
   --enable-gpl \
   --enable-version3 \
   --enable-nonfree \
-  --enable-libx264 
+  --enable-libvpx 
 
 make -j$(nproc)
 sudo make install
